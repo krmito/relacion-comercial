d3cab2a8e5fe81f0cf68cb4038afa73990d3e2d4
Jhony Renteria <jhonyrenteria@gmail.com>
2019-11-20 19:26:42 -0500

    merge con rama remota

--- src/main/java/com/p2p/relacion/entities/EntitiRelacion.java
+++ src/main/java/com/p2p/relacion/entities/EntitiRelacion.java
@@ -11,7 +11,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-import com.p2p.empresa.dto.Empresa;
+//import com.p2p.empresa.dto.Empresa;
 
 import java.io.Serializable;
 import java.security.Timestamp; 
